4edabae6d4382535f71a46ba0bce07aeb965f908
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-01-07 22:39:53 +0100

    arithmetic_operations.py script: use if instead of match-case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,15 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            result = num1 + num2
-        case 'subtract':
-            result = num1 - num2
-        case 'multiply':
-            result = num1 * num2
-        case 'divide':
-            if num2 != 0:
-                result = num1 / num2
-            else:
-                result = "we can't divide by zero"
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "we can't divide by zero"
+    else:
+        result = "Invalid operation"
     return result
