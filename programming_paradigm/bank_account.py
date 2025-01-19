bf8976a509258e9471f45cc6e66ca87019745796
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-01-19 22:51:37 +0100

    add bank account and main script

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self._account_balance = account_balance
+
+    def deposit(self, amount):
+        self._account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > self._account_balance:
+            return False
+        self._account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print(f"The account balance is ${self._account_balance}")
